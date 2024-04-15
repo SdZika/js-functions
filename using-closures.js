1f14d7d1a9ef4830a11df24d7222cf03c82678ee
SdZika <markozivkovic1982@gmail.com>
2024-04-15 22:01:13 +0200

    started using closures

---
+++ using-closures.js
@@ -0,0 +1,9 @@
+function createCounter() {
+    let myCount = 0;
+        return function() {
+            return myCount++;
+        };
+}
+
+const counter = createCounter();
+console.log(counter);
