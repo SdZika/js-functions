f696532cb8f077095b7053b2cf9d67bb90514d71
SdZika <markozivkovic1982@gmail.com>
2024-04-15 18:38:03 +0200

    done returning function

--- returning-function.js
+++ returning-function.js
@@ -1,3 +1,11 @@
-function makeMultiplier(number, operation){
-    
+function makeMultiplier(num){
+    return function(multiplier) {
+        return num * multiplier;
+    }
 }
+
+const multiplierNum = makeMultiplier(2);
+
+console.log(multiplierNum);
+
+console.log(multiplierNum(4));
